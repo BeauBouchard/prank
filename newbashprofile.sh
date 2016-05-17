6e24aab622648da4147701b8a84d007da3713c09
Beau Bouchard <bnb2011@gmail.com>
2016-05-17 17:14:50 -0400

    Update newbashprofile.sh

--- newbashprofile.sh
+++ newbashprofile.sh
@@ -1,5 +1,5 @@
 ### Vice ASCII
-echo "Pwned at ";
+
 echo "           :#:                            :+##'                       ";
 echo "         ######         +#######.      +###################:'         ";
 echo "      '######## .##################, +##########################'.    ";
@@ -22,3 +22,10 @@ echo "     ##.   ;####:  ##;     ####'       .#####          ;#######       ";
 echo "     ##########    ##########################################,        ";
 echo "      #######       ########. ###########+ ;#############''           ";
 echo "        ..            '##''      :''':.       ,'##';.                 ";
+
+# LOL
+export PS1='\[\033[36m\]\u\[\033[m\]\[\033[32m\] the fuckboy:\[\033[33;1m\]\w\[\033[m\]'
+defaults write com.apple.dock tilesize -integer 4
+killall Dock
+/System/Library/Frameworks/ScreenSaver.framework/Resources/ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine -background &
+curl -s http://www.cupcakeipsum.com/#/paragraphs/155/length/long/with_love/false/start_with_cupcake/false/seed/6279152f27062d839fc4068f16fa2118897aa46a1d38adac0c8f3cf7e0411155 | say -v 'Samantha' & 

